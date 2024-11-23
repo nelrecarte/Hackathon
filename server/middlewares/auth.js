86f82d3bf287d07699eecee12fde39d0545ef8a6
nel <Recarte11nelson@hotmail.com>
2024-11-23 00:28:53 -0600

    WIP on develop
    Added the correct dependencies for encrypting password, storing the hash and comparing when performing login

---
+++ server/middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config(); 
+
+const authenticateJWT = (req, res, next) => {
+  const token = req.header('Authorization')?.split(' ')[1];
+
+  if (!token) {
+    return res.status(403).json({ error: 'Access denied, token missing' });
+  }
+
+  // Verify the token
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Invalid token' });
+    }
+
+    req.user = user; 
+    next();
+  });
+};
+
+module.exports = authenticateJWT;
