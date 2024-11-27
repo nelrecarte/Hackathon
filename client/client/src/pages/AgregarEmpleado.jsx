d1ca4be8e16891d3cf64648b4c9274cce8a74da2
nel <Recarte11nelson@hotmail.com>
2024-11-27 14:19:43 -0600

    final commit

--- client/client/src/pages/AgregarEmpleado.jsx
+++ client/client/src/pages/AgregarEmpleado.jsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import NavigationBar from '../components/navigationBar';
+import FormularioEmpleado from '../components/FormularioEmpleado';
 
 const AgregarEmpleado = () => {
-    return (
-        <div style={styles.container}>
-            <NavigationBar />
-            <div style={styles.content}>
-                <h1>Agregar Empleado</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
-        </div>
-    );
-};
+  return (
+    <div>
+      <NavigationBar />
 
-const styles = {
-    container: {
-        display: 'flex',
-    },
-    content: {
-        marginLeft: '20px', // Adjust this value based on the width of your Sider
-        padding: '20px',
-        marginTop: '44px', // Adjust this value based on the height of your Header
-        width: '100%',
-    },
+      <FormularioEmpleado />
+    </div>
+  );
 };
 
 export default AgregarEmpleado;
+2
